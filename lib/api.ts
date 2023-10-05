7df84f302aa4a58668a11f54f21dce093f4a974a
Elias Souza <eliasnsouza1245@gmail.com>
2023-10-05 17:50:23 -0300

    .

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,10 @@
 import axios from 'axios'
 
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://fullstack-twitter-clone.vercel.app/api'
+    : 'http://localhost:3000/api'
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL,
 })
